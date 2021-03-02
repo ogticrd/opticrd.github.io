56505f7ce6d8ce5cf579c1ce3df0708b335cd3f9
JFLeonM <79935715+JFLeonM@users.noreply.github.com>
2021-03-02 17:14:03 -0400

    Update app.js

--- app.js
+++ app.js
@@ -894,15 +894,15 @@ new Vue({
             },
             {
                 name: "PUERTO PLATA", municity: [
-                    {name: "SAN FELIPE", center: [
-                        {name: "CENTRO SANITARIO FRANCISCO GONZÁLEZ HARDY"},
-                        {name: "DPS PUERTO PLATA"},
-                        {name: "PARQUE JUAN GARCÍA, SECTOR GREGORIO LUPERÓN, SAN FELIPE DE PUERTO PLATA"},
-                        {name: "CPN  SAN MARCOS"}
-                    ]},
-                                   
+                    {name: "SAN FELIPE", center: [{name: "CENTRO SANITARIO FRANCISCO GONZÁLEZ HARDY"}]},
+                    {name: "SAN FELIPE", center: [{name: "DPS PUERTO PLATA"}]},
+                    {
+                        name: "SAN FELIPE",
+                        center: [{name: "PARQUE JUAN GARCÍA, SECTOR GREGORIO LUPERÓN, SAN FELIPE DE PUERTO PLATA"}]
+                    },
                     {name: "PADRE DE LAS CASAS", center: [{name: "PLAY DEL SECTOR PADRE LAS CASAS"}]},
                     {name: "GUANANICO", center: [{name: "HOSPITAL MUNICIPAL DE GUANANICO"}]},
+                    {name: "SAN FELIPE", center: [{name: "CPN  SAN MARCOS"}]},
                     {name: "VILLA ISABELLA", center: [{name: "HOSPITAL MUNICIPAL DE VILLA ISABELA"}]}
                 ]
             },
