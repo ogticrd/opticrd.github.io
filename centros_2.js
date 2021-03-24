0cbf9a51297a251022b6c6c7f0e874f2d3f55a2e
Max Sierra <msierra@intellisys.com.do>
2021-03-24 12:48:42 -0400

    enhancement: Update centers and wording.

---
+++ centros_2.js
@@ -0,0 +1,196 @@
+const centros2 = [
+    {
+        name: "SANTO DOMINGO ESTE (DAS-I)",
+        centers: [
+            { name: "CLUB SAMEJI" },
+            { name: "TECHADO LOS CIRUELITOS" },
+            { name: "PARQUE CENTRAL" },
+            { name: "CLUB MAMBUICHE" },
+            { name: "REGIONAL DE SALUD" },
+            { name: "CLÍNICA SANTIAGO APÓSTOL" },
+            { name: "ENSANCHE LIBERTAD (GUG)" },
+            { name: "CIENFUEGOS (BAJO TECHO)" },
+            { name: "MEDICA" },
+            { name: "ZONA FRANCA PIZANO" },
+            { name: "TIPIQUITO LOS COCOS" },
+            { name: "PLAZA CORAL" },
+            { name: "DPSS 1 (2)" },
+            { name: "PARQUE VILLA GONZÁLES" },
+            { name: "CENTRO TECNOLÓGICOS" },
+        ]
+    }, {
+        name: "SAN JOSÉ DE OCOA",
+        centers: [
+            { name: "CENTRO DE DIAGNOSTICO BELLA VISTA" },
+        ]
+    }, {
+        name: "SANTIAGO (DISTRITO-I)",
+        centers: [
+            { name: "IGLESIA ADVENTISTA" },
+            { name: "CLUB SAMEJI." },
+            { name: "TECHADO LOS CIRUELITOS" },
+            { name: "CLUB MAMBUCHE" },
+            { name: "CPN HATO DEL YAQUE (CLUB)" },
+            { name: "CPN BELLA VISTA" },
+            { name: "CPN VILLA TABACALERA" },
+            { name: "HOSPITAL BELLA VISTA" },
+            { name: "CPN LA CANELA" },
+            { name: "CPN VILLA LIBERACIÓN" },
+            { name: "CPN PLATANAL" },
+            { name: "HOSPITAL HATO DEL YAQUE" },
+            { name: "LOS GUANDULES" },
+            { name: "CPN VILLA BAO" },
+            { name: "CPN VILLA TABACALERA" },
+            { name: "HOSPITAL YRENE FERNANDEZ" },
+            { name: "HOSPITAL DE SAJOMA" },
+            { name: "CPN MAMA TINGO" },
+            { name: "LA HERRADURA" },
+            { name: "SAN LUIS" },
+            { name: "LAS CHARCAS" },
+        ]
+    }, {
+        name: "SANTIAGO (DISTRITO-III)",
+        centers: [
+            { name: "REGIONAL DE EDUCACIÓN IPISA 08-03" },
+            { name: "DELIBERY,(ADULTOS MAYORES EN CAMA)" },
+            { name: "CLUB SAMEJI" },
+        ]
+    }, {
+        name: "PUERTO PLATA",
+        centers: [
+            { name: "DPS, SALUD PUBLICA" },
+            { name: "YASICA" },
+            { name: "BASE AEREA PUERTO PLATA" },
+            { name: "ARMADA, PTO PTA" },
+            { name: "IMBERT (4 PUESTO)" },
+            { name: "ALTAMIRA (4 PUESTO)" },
+            { name: "SAN FELIPE (4 PUESTO)" },
+        ]
+    }, {
+        name: "DUARTE",
+        centers: [
+            { name: "INEMED" },
+            { name: "CENTRO MÉDICO NACIONAL" },
+            { name: "SIGLO 21" },
+            { name: "CENTRO MEDICO MATERNO INFANTIL" },
+        ]
+    }, {
+        name: "LA ROMANA",
+        centers: [
+            { name: "DISPENSARIO LUIS SUAREZ" },
+            { name: "HOGAR DEL NIÑO" },
+            { name: "GOBERNACION PROVINCIAL" },
+            { name: "CENTRO DX CALETA" },
+            { name: "MASARITANO" },
+            { name: "CLINICA LOS MULOS" },
+            { name: "K-14" },
+            { name: "TOMAS DECIR" },
+            { name: "DPS LA ROMANA" },
+        ]
+    }, {
+        name: "SAMANÁ",
+        centers: [
+            { name: "CAPITANIA DE PUERTO DE SAMANA" },
+            { name: "POLIDEPORTIVO" },
+            { name: "BARRIO DANIEL WILMORES" },
+            { name: "IGLESIA EVANGELICA DOMINICANA" },
+            { name: "HONDURAS" },
+            { name: "INAIPII" },
+            { name: "AYUNTAMIENTO DE SANCHEZ" },
+            { name: "HOSPITAL DE SANCHEZ" },
+        ]
+    }, {
+        name: "BARAHONA",
+        centers: [
+            { name: "UCATEBA (SANTA CRUZ DE BARAHONA)DIST 02-03" },
+            { name: "COOPERATIVA DE MAESTROS" },
+            { name: "DIRECCIÓN REGIONAL SUR DE LA PN (SEDE)" },
+        ]
+    }, {
+        name: "BAHORUCO",
+        centers: [
+            { name: "DPS BAHORUCO" },
+        ]
+    }, {
+        name: "PEDERNALES",
+        centers: [
+            { name: "HOSPITAL DR. ELIO FIALLO" },
+            { name: "UNAP 3-4" },
+            { name: "UNAP 5-6" },
+            { name: "UNAP 7-8" },
+            { name: "UNAP 9" }
+        ]
+    }, {
+        name: "AZUA",
+        centers: [
+            { name: "LA ALTAGRACIA" },
+        ]
+    }, {
+        name: "ELÍAS PIÑA",
+        centers: [
+            { name: "HOSPITAL ROSA DUARTE" },
+            { name: "CPN RINCONCITO" },
+        ]
+    }, {
+        name: "SAN JUAN DE LA MAGUANA",
+        centers: [
+            { name: "CARDON" },
+            { name: "CASINO SAN JUAN" },
+            { name: "SALON DE CONFERENCIA DPS SAN JUAN" },
+            { name: "PEDRO CORTO" },
+            { name: "LOS COCOS" },
+            { name: "ARROYO CANO" },
+            { name: "BOHECHIO" },
+            { name: "EL CERCADO" },
+            { name: "DERRUMBADERO" },
+            { name: "VALLEJUELO" },
+        ]
+    }, {
+        name: "VALVERDE",
+        centers: [
+            { name: "COLEGIO MÉDICO" },
+            { name: "CLUB DEL CERRO" },
+            { name: "UNAP CENTRO" },
+        ]
+    }, {
+        name: "SANTIAGO RODRÍGUEZ",
+        centers: [
+            { name: "LICEO SECUNDARIO LIBRADO EUGENIO BILLIARD" },
+        ]
+    }, {
+        name: "DAJABON",
+        centers: [
+            { name: "POLIDEPORTIVO AMBRIOSIO ESTEVEZ" },
+        ]
+    }, {
+        name: "SANCHEZ RAMIREZ",
+        centers: [
+            { name: "CPN ZAMBRANA" },
+            { name: "BAJO TECHO COTUI" },
+            { name: "BAJO TECHO FANTINO" },
+            { name: "CLUB 4 DE JULIO FANTINO" },
+            { name: "CENTRO DIAGNÓSTICO FANTINO" },
+            { name: "CENTRO DIAGNÓSTICO COTUI" },
+            { name: "CPN LA BIJA" },
+            { name: "GRAN ARENA" },
+            { name: "COLEGIO MÉDICO" },
+            { name: "CPN EL LIMÓN" },
+            { name: "UTECO" },
+            { name: "HOSPITAL RAMÓN BÁEZ" },
+            { name: "HOSPITAL DE LA MATA" },
+            { name: "HOSPITAL INMACULADA CONCEPCIÓN" },
+            { name: "HOSPITAL DE CEVICOS" },
+            { name: "HOSPITAL DE FANTINO" },
+            { name: "CPN LA CRUZ COTUI" },
+            { name: "CPN LA ALTAGRACIA COTUI" },
+            { name: "CPN LIBERTAD" },
+            { name: "DPS" },
+            { name: "CPN LA MATA" },
+            { name: "CPN LA CUEVA" },
+            { name: "CPN LIMONCITO" },
+            { name: "CPN EL PESCOZON" },
+            { name: "CPN LA PLACETA" },
+            { name: "CPN PEDRO FRANCISCO BONO" },
+        ]
+    },
+].sort((a, b) => a.name < b.name ? -1 : 1);
