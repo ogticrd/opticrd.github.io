eff902f6cd0331335ff5182448ba9e64764217fa
David Minaya <davidminaya04@gmail.com>
2021-03-28 17:05:06 -0400

    Update the centers

---
+++ centros_4.js
@@ -0,0 +1,424 @@
+const centros = [
+  {
+    name: "San Francisco De Macoris",
+    centers: [
+      { name: "Grupo Medico Unido Siglo 21" },
+      { name: "Centro Medico Materno Infantil del Nordeste" },
+      { name: "Centro Medico Nacional" },
+      { name: "Centro Medico de Especialidades Medicas (INEMED)" },
+      { name: "Fortaleza Duarte" },
+      { name: "Policia Nacional" },
+      { name: "Hospital Regional San Vicente de Paul" },
+      { name: "CIPLACI" },
+      { name: "Policia Nacional" },
+      { name: "Centro del Jaya " },
+      { name: "Capilla La Altagracia" },
+      { name: "Ruta CPN 1A" },
+      { name: "Ruta CPN 1B" },
+      { name: "Ruta CPN 1C" },
+      { name: "Ruta CPN 2A" },
+      { name: "Ruta CPN 2B" },
+      { name: "Ruta CPN 3" },
+      { name: "Ruta CPN 4" },
+      { name: "Ruta CPN 5" },
+      { name: "Ruta CPN 6" },
+      { name: "Ruta CPN 7" },
+      { name: "Ruta CPN 8" },
+      { name: "UCNE (Intrnos de Medicina)" },
+      { name: "Hogar Geriatrico Niña  Valerio " },
+      { name: "Casa Senatorial " },
+      { name: "Club de Mayorista" },
+      { name: "UASD (Internos de Medicina)" },
+      { name: "Centro Medico Dr. Ovalle " },
+      { name: "Centro Medico Dr. Nuñez " },
+      { name: "Clinica Dra. Flabia Sanchez (SAREI)" },
+      { name: "Clinica Dr. Sarante " },
+      { name: "Dr. Reynoso " },
+      { name: "Dr. Goris Abreu " },
+      { name: "Cardio Renal " },
+      { name: "Cruz Roja  Dominicana " },
+      { name: "Centro Israel Garcia" },
+      { name: "CEDINEFROT " },
+      { name: "Incardio " },
+      { name: "Fundacion de Cardiologia " },
+      { name: "Rehabilitacion " },
+      { name: "Yun Peralta " },
+      { name: "Oncologico " },
+      { name: "Serenidad " },
+      { name: "Casa Senatorial" },
+      { name: "Club de Mayorista" },
+      { name: "Club de los Profesores" },
+      { name: "Centro Comunal Pablo VI" },
+      { name: "Club 27 de Febrero " },
+      { name: "Capilla La Altagracia" },
+      { name: "Cancha San Martin" },
+      { name: "Ciudad Ganadera" },
+      { name: "Centro Medico Dr. Ovalle " },
+      { name: "Grupo Medico Unido Siglo 21" },
+      { name: "Casa Senatorial " },
+      { name: "Club de Mayorista" },
+      { name: "Club 27 de Febrero " },
+      { name: "Grupo Medico Unido Siglo 21" },
+      { name: "Centro Medico Dr. Ovalle " },
+      { name: "Centro Comunal Pablo VI" },
+      { name: "Cancha San Martin " },
+      { name: "Club de los Profesores" },
+      { name: "Capilla La Altagracia " },
+      { name: "Ciudad Ganadera " },
+      { name: "Getsemani " },
+      { name: "UASD (Internos de Medicina)" },
+      { name: "Casa Senatorial" },
+      { name: "Club de Mayorista" },
+      { name: "Club Olegario Tenares (Vista al Valle)" },
+      { name: "Ciudad Ganadera" },
+      { name: "Cancha San Martin" },
+      { name: "Profamilia " },
+      { name: "UCNE (Intrnos de Medicina)" },
+      { name: "Club Cristo Rey" },
+      { name: "Grupo Medico Unido Siglo 21" },
+      { name: "Centro Comunal Pablo VI" },
+      { name: "DPS Duarte" },
+      { name: "Capilla La Altagracia" },
+      { name: "Casa Senatorial" },
+      { name: "Club de Mayorista" },
+      { name: "Centro Comunal San Juan Bautista" },
+      { name: "UCNE (Intrnos de Medicina)" },
+      { name: "Club Cristo Rey " },
+      { name: "Grupo Medico Unido Siglo 21" },
+      { name: "Centro Medico Dr. Ovalle " },
+      { name: "Capilla La Altagracia " },
+      { name: "DPS Duarte" },
+      { name: "Los Espinolas " },
+      { name: "Hermanas Mirabal " },
+      { name: "Santa Ana " },
+      { name: "Fortaleza Duarte  (911)" },
+      { name: "Regional de Salud " },
+      { name: "UASD (Internos de Medicina)" }
+    ]
+  },
+  {
+    name: "Aguayo",
+    centers: [
+      { name: "Centro Comunal de Aguayo" }
+    ],
+  },
+  {
+    name: "Arenoso",
+    centers: [
+      { name: "Hospital Municipal Arenoso" },
+      { name: "Multiuso Bajo Techo Arenoso" },
+      { name: "Bajo Techo de Arenoso " },
+      { name: "Multiuso Bajo Techo Arenoso" }
+    ],
+  },
+  {
+    name: "Castillo",
+    centers: [
+      { name: "Hospital Municipal Castillo" },
+      { name: "Centro Comunal de Castillo" }
+    ],
+  },
+  {
+    name: "Cenovi",
+    centers: [
+      { name: "Hospital Dr.  Mario Fernandez Mena" },
+      { name: "Centro Comunal  Salome Ureña (Cenovi)" }
+    ],
+  },
+  {
+    name: "Hostos",
+    centers: [
+      { name: "Centro Comunal de Hostos " }
+    ],
+  },
+  {
+    name: "Hoyo de Jaya",
+    centers: [
+      { name: "Casa Curial (Hoyo de Jaya)" }
+    ],
+  },
+  {
+    name: "La Peña",
+    centers: [
+      { name: "Club Miguel Gelaber" }
+    ],
+  },
+  {
+    name: "Las Guaranas",
+    centers: [
+      { name: "Hospital Municipal Ethailda Brito" },
+      { name: "Club Blue" }
+    ],
+  },
+  {
+    name: "Pimentel",
+    centers: [
+      { name: "Hospital Municipal Felipe J. Achecar" },
+      { name: "Arena 57 " }
+    ],
+  },
+  {
+    name: "Villa Rivas",
+    centers: [
+      { name: "Hospital Municipal Alicia Legendre" },
+      { name: "Multiuso Bajo Techo Villa Rivas" },
+      { name: "Bajo Techo Villa Riva" },
+      { name: "Villa rivas ( Guaraguao)" }
+    ],
+  },
+  {
+    name: "Santa Barbara",
+    centers: [
+      { name: "Polideportivo" },
+      { name: "Centro Tecnologico del Limon" },
+      { name: "Centro Comunal Los Robalos" },
+      { name: "Cruce de Honduras (Edificio blanco)" },
+      { name: "Centro Comunal Rancho Español" },
+      { name: "FABRICA DE HIELO LA PASCUALA" },
+      { name: "CRUCE DE TEZON" },
+      { name: "AYUNTAMIENTO LAS GALERAS" },
+      { name: "JUNTA DE VECINO DANIEL WILLMORE" },
+      { name: "Iglesia Catolica " },
+      { name: "Sindicato de portuaria " },
+      { name: "Junta de vecinos Los Cacaos" },
+      { name: "Local Cooperativa vieja " }
+    ],
+  },
+  {
+    name: "Las Terrenas",
+    centers: [
+      { name: "Centro Comunal" },
+      { name: "Iglesia Catolica" },
+      { name: "CLUB DE MADRES LOS PUENTES" },
+      { name: "Polideportivo " }
+    ],
+  },
+  {
+    name: "Sanchez",
+    centers: [
+      { name: "CPN La Lometa" },
+      { name: "Centro Comunal, Batey Hormiga" },
+      { name: "MAJAGUA ADENTRO" },
+      { name: "Iglesia Catolica Las Garitas" },
+      { name: "Centro Comunal Los Corrales" },
+      { name: "Cruz Roja Dominicana" },
+      { name: "Ayuntamiento Municipal" },
+      { name: "Hospital Dr. Alberto Gautreaux" }
+    ],
+  },
+  {
+    name: "Salcedo",
+    centers: [
+      { name: "HOSPITAL DR. PASACASIO TORIBIO PIANTINI" },
+      { name: "CENTRO MEDICO MATERNO INFANTIL" },
+      { name: "CENTRO MEDICO Dr.GRULLON" },
+      { name: "HOSPITAL Dr.ROMAN BRACHEZ" },
+      { name: "CASA DE LOS JOVENES DE AYER" },
+      { name: "LOCAL DEL COLEGIO MEDICO PROVINCIAL" },
+      { name: "UNAP  ANA MERCEDES RODRIGUEZ" },
+      { name: "CASA DE LA TERCERA EDAD" }
+    ],
+  },
+  {
+    name: "Tenares",
+    centers: [
+      { name: "HOSPITAL ANGEL CONCEPCION LAJARA" },
+      { name: "CENTRO MEDICO SAN RAFAEL" },
+      { name: "UNIVERSAL DE SALUD" },
+      { name: "CENTRO MEDICO DR. GUZMAN" },
+      { name: "CENTRO DIAGNOSTICO TENARES" },
+      { name: "POLIDEPORTIVO TENARES" },
+      { name: "CENTRO COMUNITARIO PASO HONDO ADULTOS DE 70 AÑOS" },
+      { name: "CENTRO COMUNITARIO LA JAGUITA TENARES ADULTOS DE 70 AÑOS Y MAS" },
+      { name: "UNAP EULALIA PEÑA PERSONAL" }
+    ],
+  },
+  {
+    name: "Villa Tapia",
+    centers: [
+      { name: "HOSPITAL MUNICIPAL VILLA TAPIA" },
+      { name: "CENTRO DIAGNOSTICO " },
+      { name: "CENTRO MEDICO DE ESPECIALIDADES DE VILLA TAPIA (CEMEVIT)" },
+      { name: "HOGAR PADRE NOEL VILLA TAPIA" },
+      { name: "PUESTO MOVIL HOGAR DE ANCIANO PADRE NOEL" }
+    ],
+  },
+  {
+    name: "Tamayo",
+    centers: [
+      { name: "HOSPITAL MUNICIPAL JULIA SANTANA" },
+      { name: "AYUNTAMIENTO DE TAMAYO" },
+      { name: "TECHADO DE TAMAYO" },
+      { name: "MONSERRATE " }
+    ],
+  },
+  {
+    name: "Neyba",
+    centers: [
+      { name: "HOSP. SAN BARTOLOME" },
+      { name: "MULTIUSO NEYBA" },
+      { name: "HOGAR ALEGRIA" },
+      { name: "COLEGIO MEDICO (NEYBA)" },
+      { name: "DPS BAHORUCO" },
+      { name: "EL MANGUITO" }
+    ],
+  },
+  {
+    name: "Los Rios",
+    centers: [
+      { name: "CLUB LAS VIÑAS" },
+      { name: "HOSP. MUNICIPAL LOS RIOS" }
+    ],
+  },
+  {
+    name: "Galvan",
+    centers: [
+      { name: "ESC. LAS TEJAS" },
+      { name: "CENTRO COMUNAL GALVAN" },
+      { name: "ESC. ADELCIDA PEÑA MENDEZ" }
+    ],
+  },
+  {
+    name: "Villa Jaragua",
+    centers: [
+      { name: "TECHADO DE JARAGUA" },
+      { name: "ESC. FE Y ALEGRIA" },
+      { name: "HOGAR DIA VILLA JARAGUA" }
+    ],
+  },
+  {
+    name: "Las Clavellinas",
+    centers: [
+      { name: "ESC. MAXIMO GOMEZ" }
+    ],
+  },
+  {
+    name: "Santa Cruz",
+    centers: [
+      { name: "Iglesia Resureccion y vida" },
+      { name: "Iglesia Buenas Nuevas" },
+      { name: "Iglesia de Dios Pentecostal M.I" },
+      { name: "Colegio Medico Dominicano" },
+      { name: "CMD Filiar Barahona" }
+    ],
+  },
+  {
+    name: "Distrito Municipal de Villa Central",
+    centers: [
+      { name: "Club Juan Pablo Duarte" }
+    ],
+  },
+  {
+    name: "Vicente Noble",
+    centers: [
+      { name: "Club Cultural Vicente Noble" }
+    ],
+  },
+  {
+    name: "Cabral",
+    centers: [
+      { name: "Polideportivo/Multiusos Cabral" },
+      { name: "Club Comunitario Las Viñas" }
+    ],
+  },
+  {
+    name: "Polo",
+    centers: [
+      { name: "Club Municipal de Polo" }
+    ],
+  },
+  {
+    name: "El Cachón",
+    centers: [
+      { name: "Club el Cachon" }
+    ],
+  },
+  {
+    name: "Enriquillo",
+    centers: [
+      { name: "Ayuntamiento de Enriquillo" }
+    ],
+  },
+  {
+    name: "Paraiso",
+    centers: [
+      { name: "Ayuntamiento de Paraiso" }
+    ],
+  },
+  {
+    name: "La Cienaga",
+    centers: [
+      { name: "Centro Comunitario de La Cienaga" }
+    ],
+  },
+  {
+    name: "Provincia de Barahona",
+    centers: [
+      { name: "Todas las unidades de Atencion Primaria  de la Provincia de Barahona, que son 40." }
+    ],
+  },
+  {
+    name: "Jimani",
+    centers: [
+      { name: "Centro Diagnostico Jimani" },
+      { name: "Defensa Civil Jimani" },
+      { name: "CPN Jimani Viejo" },
+      { name: "CPN Jimani Pueblo" },
+      { name: "CPN El Limon" },
+      { name: "CPN Boca De Cachon" },
+      { name: "CPN Tierra Nueva" }
+    ],
+  },
+  {
+    name: "Duverge",
+    centers: [
+      { name: "CPN Las Baitoas" },
+      { name: "CPN Vengan a Ver" },
+      { name: "CPN Puesto Escondido" },
+      { name: "CPN San Jose" },
+      { name: "CPN Las Mercedes" },
+      { name: "CPN San Isidro" },
+      { name: "Hosp.  Munic. DR. Jose Perez" },
+      { name: "CPN Placer Bonito" }
+    ],
+  },
+  {
+    name: "Mella",
+    centers: [
+      { name: "CPN Colonia Mixta" },
+      { name: "CPN Mella " }
+    ],
+  },
+  {
+    name: "Angostura",
+    centers: [
+      { name: "CPN Angostura" }
+    ],
+  },
+  {
+    name: "Cristobal",
+    centers: [
+      { name: "CPN Cristobal" },
+      { name: "CPN Batey -8" }
+    ],
+  },
+  {
+    name: "La Descubierta",
+    centers: [
+      { name: "CPN Bartolome" },
+      { name: "CPN Los Pinos Del Eden" },
+      { name: "Hosp. Munic. La Descubierta" },
+      { name: "CPN La Descubierta" }
+    ],
+  },
+  {
+    name: "Postrer Rio",
+    centers: [
+      { name: "CPN Postrer Rio" },
+      { name: "CPN El Guayabal" },
+      { name: "CPN Los Bolos" }
+    ],
+  }
+];
+
+centros.sort((a, b) => (a.name < b.name ? -1 : 1));
